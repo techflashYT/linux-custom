xerpi/linux_vita

ARM: vita: Reset controller, SPI, GPIO, Syscon, Syscon buttons & touchscreen

drivers/mfd/vita-syscon.c
@@ -1,0 +1,233 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2021 Sergi Granell
+
+#include <linux/module.h>
+#include <linux/interrupt.h>
+#include <linux/spi/spi.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/of_irq.h>
+#include <linux/mfd/core.h>
+#include <linux/mfd/vita-syscon.h>
+
+static const struct mfd_cell vita_syscon_devs[] = {
+	{
+		.name = "vita-syscon-ts",
+		.of_compatible = "vita,syscon-ts"
+	},
+	{
+		.name = "vita-syscon-buttons",
+		.of_compatible = "vita,syscon-buttons"
+	},
+};
+
+static inline void syscon_set_tx_gpio(struct vita_syscon *syscon, int is_on)
+{
+	gpiod_set_value_cansleep(syscon->tx_gpio, is_on);
+}
+
+static inline void syscon_calc_checksum(u8 *tx, int data_size)
+{
+	int i;
+	u32 hash = 0;
+
+	for (i = 0; i < data_size; i++)
+		hash += tx[i];
+
+	tx[data_size] = ~hash;
+}
+
+static int vita_syscon_transfer(struct vita_syscon *syscon, u8 *tx, void *rx, int rx_size)
+{
+	struct spi_message msg;
+	struct spi_transfer tx_xfer, rx_xfer;
+	struct spi_device *spi = syscon->spi;
+	unsigned int timeout;
+	int ret = 0;
+	int tx_size = SYSCON_TX_HEADER_SIZE + tx[SYSCON_TX_LENGTH];
+	u8 res;
+
+	memset(&tx_xfer, 0, sizeof(tx_xfer));
+	memset(&rx_xfer, 0, sizeof(rx_xfer));
+
+	tx_xfer.tx_buf = tx;
+	tx_xfer.len = tx_size;
+	rx_xfer.rx_buf = rx;
+	rx_xfer.len = rx_size;
+
+	syscon_calc_checksum(tx, tx_size - 1);
+
+	spi_bus_lock(spi->master);
+
+	do {
+		reinit_completion(&syscon->rx_irq);
+
+		syscon_set_tx_gpio(syscon, true);
+
+		/* Send data */
+		spi_message_init(&msg);
+		spi_message_add_tail(&tx_xfer, &msg);
+		ret = spi_sync_locked(spi, &msg);
+		if (ret < 0) {
+			syscon_set_tx_gpio(syscon, false);
+			goto out;
+		}
+
+		syscon_set_tx_gpio(syscon, false);
+
+		/* Wait RX interrupt */
+		timeout = wait_for_completion_timeout(&syscon->rx_irq,
+						      msecs_to_jiffies(250));
+		if (!timeout) {
+			dev_warn(&spi->dev, "%s: RX IRQ timeout", dev_name(&spi->dev));
+			ret = -ETIMEDOUT;
+			goto out;
+		}
+
+		/* Receive data */
+		spi_message_init(&msg);
+		spi_message_add_tail(&rx_xfer, &msg);
+		ret = spi_sync_locked(spi, &msg);
+		if (ret < 0)
+			goto out;
+		res = ((u8 *)rx)[SYSCON_RX_RESULT];
+	} while (res == 0x80 || res == 0x81);
+
+out:
+	spi_bus_unlock(spi->master);
+
+	return ret;
+}
+
+static int vita_syscon_command_read(struct vita_syscon *syscon, u16 cmd, void *rx, int rx_size)
+{
+	u8 tx[SYSCON_TX_HEADER_SIZE + 1];
+
+	tx[SYSCON_TX_CMD_LO] = cmd & 0xFF;
+	tx[SYSCON_TX_CMD_HI] = (cmd >> 8) & 0xFF;
+	tx[SYSCON_TX_LENGTH] = 1;
+
+	return vita_syscon_transfer(syscon, tx, rx, rx_size);
+}
+
+static int vita_syscon_short_command_write(struct vita_syscon *syscon, u16 cmd, u32 data, int cmd_len)
+{
+	u8 tx[SYSCON_TX_HEADER_SIZE + sizeof(u32) + 1];
+	u8 rx[16];
+
+	tx[SYSCON_TX_CMD_LO] = cmd & 0xFF;
+	tx[SYSCON_TX_CMD_HI] = (cmd >> 8) & 0xFF;
+	tx[SYSCON_TX_LENGTH] = cmd_len;
+
+	tx[SYSCON_TX_DATA(0)] = data & 0xFF;
+	tx[SYSCON_TX_DATA(1)] = (data >> 8) & 0xFF;
+	tx[SYSCON_TX_DATA(2)] = (data >> 16) & 0xFF;
+	tx[SYSCON_TX_DATA(3)] = (data >> 24) & 0xFF;
+
+	return vita_syscon_transfer(syscon, tx, rx, sizeof(rx));
+}
+
+static irqreturn_t vita_syscon_rx_gpio_irq_handler(int irq, void *dev_id)
+{
+	struct vita_syscon *syscon = dev_id;
+
+	complete(&syscon->rx_irq);
+
+	return IRQ_HANDLED;
+}
+
+static int vita_syscon_probe(struct spi_device *spi)
+{
+	struct vita_syscon *syscon;
+	u8 baryon_version[SYSCON_RX_HEADER_SIZE + sizeof(syscon->baryon_version)];
+	u8 hw_info[SYSCON_RX_HEADER_SIZE + sizeof(syscon->hardware_info)];
+	u8 hw_flags[SYSCON_RX_HEADER_SIZE + sizeof(syscon->hardware_flags)];
+	int ret, irq;
+
+	pr_info("vita_syscon_probe\n");
+
+	syscon = devm_kzalloc(&spi->dev, sizeof(struct vita_syscon),
+				GFP_KERNEL);
+	if (!syscon)
+		return -ENOMEM;
+
+	irq = of_irq_get(spi->dev.of_node, 0);
+	if (irq <= 0)
+		return irq ? irq : -ENODEV;
+
+	ret = devm_request_irq(&spi->dev, irq, vita_syscon_rx_gpio_irq_handler, 0,
+			       "vita-syscon-gpio-rx", syscon);
+	if (ret) {
+		dev_err(&spi->dev, "could not request IRQ: %d\n", ret);
+		return ret;
+	}
+
+	syscon->tx_gpio = devm_gpiod_get(&spi->dev, "tx", GPIOD_OUT_LOW);
+	if (IS_ERR(syscon->tx_gpio))
+		return PTR_ERR(syscon->tx_gpio);
+
+	init_completion(&syscon->rx_irq);
+
+	spi_set_drvdata(spi, syscon);
+	syscon->dev = &spi->dev;
+	syscon->spi = spi;
+	syscon->command_read = vita_syscon_command_read;
+	syscon->short_command_write = vita_syscon_short_command_write;
+	syscon->transfer = vita_syscon_transfer;
+
+	ret = vita_syscon_command_read(syscon, 1, baryon_version, sizeof(baryon_version));
+	if (ret < 0) {
+		return ret;
+	}
+	memcpy(&syscon->baryon_version, &baryon_version[SYSCON_RX_DATA],
+	       sizeof(syscon->baryon_version));
+
+	pr_info("Vita Syscon Baryon version: 0x%X\n", syscon->baryon_version);
+
+	if (syscon->baryon_version > 0x1000003)
+		ret = vita_syscon_short_command_write(syscon, 0x80, 0x12, 3);
+	else if (syscon->baryon_version > 0x70501)
+		ret = vita_syscon_short_command_write(syscon, 0x80, 2, 3);
+	if (ret < 0) {
+		return ret;
+	}
+
+	ret = vita_syscon_command_read(syscon, 5, hw_info, sizeof(hw_info));
+	if (ret < 0) {
+		return ret;
+	}
+	memcpy(&syscon->hardware_info, &hw_info[SYSCON_RX_DATA], sizeof(syscon->hardware_info));
+
+	pr_info("Vita Syscon HW info: 0x%X\n", syscon->hardware_info);
+
+	ret = vita_syscon_command_read(syscon, 6, hw_flags, sizeof(hw_flags));
+	if (ret < 0) {
+		return ret;
+	}
+	memcpy(syscon->hardware_flags, &hw_flags[SYSCON_RX_DATA], sizeof(syscon->hardware_flags));
+
+	return devm_mfd_add_devices(syscon->dev, PLATFORM_DEVID_NONE,
+				    vita_syscon_devs, ARRAY_SIZE(vita_syscon_devs),
+				    NULL, 0, NULL);
+}
+
+static const struct of_device_id vita_syscon_of_match[] = {
+	{ .compatible = "vita,syscon" },
+	{}
+};
+
+MODULE_DEVICE_TABLE(of, vita_syscon_of_match);
+
+static struct spi_driver vita_syscon_driver = {
+	.driver = {
+		.name   = "vita-syscon",
+		.of_match_table = vita_syscon_of_match,
+	},
+	.probe = vita_syscon_probe,
+};
+
+module_spi_driver(vita_syscon_driver);
+
+MODULE_AUTHOR("Sergi Granell");
+MODULE_DESCRIPTION("PlayStation Vita's Syscon (Ernie) driver");
+MODULE_LICENSE("GPL");

drivers/spi/spi-vita.c
@@ -1,0 +1,240 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2021 Sergi Granell
+
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/device.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
+#include <linux/reset.h>
+#include <linux/spi/spi.h>
+
+#define SPI_RXFIFO	0x00
+#define SPI_TXFIFO	0x04
+#define SPI_CONTROL	0x08
+#define SPI_INT_CTL	0x0C
+#define SPI_STATUS	0x10
+#define SPI_UNK20	0x20
+#define SPI_INT_STAT	0x24
+#define SPI_RXFIFO_STAT	0x28
+#define SPI_TXFIFO_STAT	0x2C
+
+struct vita_spi {
+	struct spi_controller	*ctlr;
+	void __iomem		*regs;
+	struct clk		*clk;
+	struct reset_control	*rst;
+};
+
+static inline u32 vita_spi_read(struct vita_spi *vspi, u32 reg)
+{
+	u32 val = readl(vspi->regs + reg);
+	//pr_info("vita_spi_read: reg: 0x%02X, value: 0x%X\n", reg, val);
+	return val;
+}
+
+static inline void vita_spi_write(struct vita_spi *vspi, u32 reg, u32 value)
+{
+	//pr_info("vita_spi_write: reg: 0x%02X, value: 0x%X\n", reg, value);
+	writel(value, vspi->regs + reg);
+}
+
+static inline u32 vita_spi_read_fifo_available(struct vita_spi *vspi)
+{
+	return vita_spi_read(vspi, SPI_RXFIFO_STAT) & 0x7F;
+}
+
+static void vita_spi_transfer_send(struct vita_spi *vspi, const u8 *tx_buf, int len)
+{
+	u32 val;
+	unsigned int i = 0;
+
+	/* Flush pending data in the RX FIFO */
+	while (vita_spi_read_fifo_available(vspi) > 0)
+		vita_spi_read(vspi, SPI_RXFIFO);
+
+	vita_spi_read(vspi, SPI_TXFIFO_STAT);
+	vita_spi_write(vspi, SPI_INT_STAT, 0x600);
+
+	/* Transfer data */
+	while (len >= 2) {
+		val = tx_buf[i] | ((u32)tx_buf[i + 1] << 8);
+		vita_spi_write(vspi, SPI_TXFIFO, val);
+		i += 2;
+		len -= 2;
+	}
+
+	if (len > 0)
+		vita_spi_write(vspi, SPI_TXFIFO, tx_buf[i]);
+
+	/* Transfer end */
+	vita_spi_write(vspi, SPI_CONTROL, 0);
+	vita_spi_write(vspi, SPI_STATUS, 1);
+	vita_spi_read(vspi, SPI_STATUS);
+}
+
+static void vita_spi_transfer_recv(struct vita_spi *vspi, u8 *rx_buf, int len)
+{
+	u32 val, avail;
+	int i = 0;
+
+	while (i < len) {
+		avail = vita_spi_read_fifo_available(vspi);
+		if (!avail)
+			break;
+
+		val = vita_spi_read(vspi, SPI_RXFIFO);
+
+		if (avail >= 2 && ((i + 2) <= len)) {
+			rx_buf[i] = val & 0xFF;
+			rx_buf[i + 1] = (val >> 8) & 0xFF;
+			i += 2;
+		} else if (avail >= 1) {
+			rx_buf[i] = val & 0xFF;
+			i++;
+		}
+	}
+
+	/* Transfer end */
+	vita_spi_write(vspi, SPI_STATUS, 0);
+	vita_spi_read(vspi, SPI_STATUS);
+}
+
+static int vita_spi_transfer_one(struct spi_controller *ctlr,
+				 struct spi_device *spi,
+				 struct spi_transfer *tfr)
+{
+
+	struct vita_spi *vspi = spi_controller_get_devdata(ctlr);
+
+	//pr_info("vita_spi_transfer_one. tx_buf: 0x%X, rx_buf: 0x%X, len: 0x%X\n",
+	//       tfr->tx_buf, tfr->rx_buf, tfr->len);
+
+	if (tfr->rx_buf) {
+		vita_spi_transfer_recv(vspi, tfr->rx_buf, tfr->len);
+	} else {
+		vita_spi_transfer_send(vspi, tfr->tx_buf, tfr->len);
+	}
+
+	return 0;
+}
+
+static int vita_spi_probe(struct platform_device *pdev)
+{
+	struct spi_controller *ctlr;
+	struct vita_spi *vspi;
+	struct device *dev = &pdev->dev;
+	int ret = 0;
+
+	pr_info("vita_spi_probe\n");
+
+	ctlr = devm_spi_alloc_master(dev, sizeof(struct vita_spi));
+	if (!ctlr)
+		return -ENOMEM;
+
+	platform_set_drvdata(pdev, ctlr);
+
+	ctlr->transfer_one = vita_spi_transfer_one;
+	ctlr->mode_bits = SPI_CPOL | SPI_CPHA | SPI_CS_HIGH | SPI_LSB_FIRST;
+	ctlr->bits_per_word_mask = SPI_BPW_MASK(8);
+	ctlr->dev.of_node = pdev->dev.of_node;
+	ctlr->auto_runtime_pm = true;
+
+	vspi = spi_controller_get_devdata(ctlr);
+	vspi->ctlr = ctlr;
+
+	vspi->regs = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(vspi->regs))
+		return PTR_ERR(vspi->regs);
+
+	vspi->clk = devm_clk_get_optional(dev, NULL);
+	if (IS_ERR(vspi->clk))
+		return dev_err_probe(dev, PTR_ERR(vspi->clk),
+				     "could not get clk\n");
+
+	clk_prepare_enable(vspi->clk);
+
+	vspi->rst = devm_reset_control_get_exclusive(dev, NULL);
+	if (IS_ERR(vspi->rst)) {
+		ret = PTR_ERR(vspi->rst);
+		goto err_disable_clk;
+	}
+	reset_control_deassert(vspi->rst);
+
+	vita_spi_write(vspi, SPI_UNK20, 0);
+	vita_spi_read(vspi, SPI_UNK20);
+
+	pm_runtime_enable(dev);
+	pm_runtime_set_active(dev);
+
+	ret = spi_register_controller(ctlr);
+	if (ret) {
+		dev_err(dev, "could not register SPI controller: %d\n",
+			ret);
+		goto err_reset;
+	}
+
+	return 0;
+
+err_reset:
+	reset_control_assert(vspi->rst);
+
+err_disable_clk:
+	clk_disable_unprepare(vspi->clk);
+
+	return ret;
+}
+
+static int vita_spi_remove(struct platform_device *pdev)
+{
+	struct spi_controller *ctlr = platform_get_drvdata(pdev);
+	struct vita_spi *vspi = spi_controller_get_devdata(ctlr);
+	struct device *dev = &pdev->dev;
+
+	pm_runtime_get_sync(dev);
+
+	reset_control_assert(vspi->rst);
+
+	clk_disable_unprepare(vspi->clk);
+
+	pm_runtime_disable(dev);
+	pm_runtime_put_noidle(dev);
+
+	spi_unregister_controller(ctlr);
+
+	return 0;
+}
+
+static void vita_spi_shutdown(struct platform_device *pdev)
+{
+	int ret;
+
+	ret = vita_spi_remove(pdev);
+	if (ret)
+		dev_err(&pdev->dev, "failed to shutdown\n");
+}
+
+static const struct of_device_id vita_spi_match[] = {
+	{ .compatible = "vita,spi", },
+	{}
+};
+MODULE_DEVICE_TABLE(of, vita_spi_match);
+
+static struct platform_driver vita_spi_driver = {
+	.driver	= {
+		.name		= "vita-spi",
+		.of_match_table	= vita_spi_match,
+	},
+	.probe		= vita_spi_probe,
+	.remove		= vita_spi_remove,
+	.shutdown	= vita_spi_shutdown,
+};
+module_platform_driver(vita_spi_driver);
+
+MODULE_AUTHOR("Sergi Granell");
+MODULE_DESCRIPTION("PlayStation Vita SPI driver");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:vita-spi");

techflashYT/linux-custom

drivers/mfd/vita-syscon.c
@@ -1,0 +1,233 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2021 Sergi Granell
+
+#include <linux/module.h>
+#include <linux/interrupt.h>
+#include <linux/spi/spi.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/of_irq.h>
+#include <linux/mfd/core.h>
+#include <linux/mfd/vita-syscon.h>
+
+static const struct mfd_cell vita_syscon_devs[] = {
+	{
+		.name = "vita-syscon-ts",
+		.of_compatible = "vita,syscon-ts"
+	},
+	{
+		.name = "vita-syscon-buttons",
+		.of_compatible = "vita,syscon-buttons"
+	},
+};
+
+static inline void syscon_set_tx_gpio(struct vita_syscon *syscon, int is_on)
+{
+	gpiod_set_value_cansleep(syscon->tx_gpio, is_on);
+}
+
+static inline void syscon_calc_checksum(u8 *tx, int data_size)
+{
+	int i;
+	u32 hash = 0;
+
+	for (i = 0; i < data_size; i++)
+		hash += tx[i];
+
+	tx[data_size] = ~hash;
+}
+
+static int vita_syscon_transfer(struct vita_syscon *syscon, u8 *tx, void *rx, int rx_size)
+{
+	struct spi_message msg;
+	struct spi_transfer tx_xfer, rx_xfer;
+	struct spi_device *spi = syscon->spi;
+	unsigned int timeout;
+	int ret = 0;
+	int tx_size = SYSCON_TX_HEADER_SIZE + tx[SYSCON_TX_LENGTH];
+	u8 res;
+
+	memset(&tx_xfer, 0, sizeof(tx_xfer));
+	memset(&rx_xfer, 0, sizeof(rx_xfer));
+
+	tx_xfer.tx_buf = tx;
+	tx_xfer.len = tx_size;
+	rx_xfer.rx_buf = rx;
+	rx_xfer.len = rx_size;
+
+	syscon_calc_checksum(tx, tx_size - 1);
+
+	spi_bus_lock(spi->controller);
+
+	do {
+		reinit_completion(&syscon->rx_irq);
+
+		syscon_set_tx_gpio(syscon, true);
+
+		/* Send data */
+		spi_message_init(&msg);
+		spi_message_add_tail(&tx_xfer, &msg);
+		ret = spi_sync_locked(spi, &msg);
+		if (ret < 0) {
+			syscon_set_tx_gpio(syscon, false);
+			goto out;
+		}
+
+		syscon_set_tx_gpio(syscon, false);
+
+		/* Wait RX interrupt */
+		timeout = wait_for_completion_timeout(&syscon->rx_irq,
+						      msecs_to_jiffies(250));
+		if (!timeout) {
+			dev_warn(&spi->dev, "%s: RX IRQ timeout", dev_name(&spi->dev));
+			ret = -ETIMEDOUT;
+			goto out;
+		}
+
+		/* Receive data */
+		spi_message_init(&msg);
+		spi_message_add_tail(&rx_xfer, &msg);
+		ret = spi_sync_locked(spi, &msg);
+		if (ret < 0)
+			goto out;
+		res = ((u8 *)rx)[SYSCON_RX_RESULT];
+	} while (res == 0x80 || res == 0x81);
+
+out:
+	spi_bus_unlock(spi->controller);
+
+	return ret;
+}
+
+static int vita_syscon_command_read(struct vita_syscon *syscon, u16 cmd, void *rx, int rx_size)
+{
+	u8 tx[SYSCON_TX_HEADER_SIZE + 1];
+
+	tx[SYSCON_TX_CMD_LO] = cmd & 0xFF;
+	tx[SYSCON_TX_CMD_HI] = (cmd >> 8) & 0xFF;
+	tx[SYSCON_TX_LENGTH] = 1;
+
+	return vita_syscon_transfer(syscon, tx, rx, rx_size);
+}
+
+static int vita_syscon_short_command_write(struct vita_syscon *syscon, u16 cmd, u32 data, int cmd_len)
+{
+	u8 tx[SYSCON_TX_HEADER_SIZE + sizeof(u32) + 1];
+	u8 rx[16];
+
+	tx[SYSCON_TX_CMD_LO] = cmd & 0xFF;
+	tx[SYSCON_TX_CMD_HI] = (cmd >> 8) & 0xFF;
+	tx[SYSCON_TX_LENGTH] = cmd_len;
+
+	tx[SYSCON_TX_DATA(0)] = data & 0xFF;
+	tx[SYSCON_TX_DATA(1)] = (data >> 8) & 0xFF;
+	tx[SYSCON_TX_DATA(2)] = (data >> 16) & 0xFF;
+	tx[SYSCON_TX_DATA(3)] = (data >> 24) & 0xFF;
+
+	return vita_syscon_transfer(syscon, tx, rx, sizeof(rx));
+}
+
+static irqreturn_t vita_syscon_rx_gpio_irq_handler(int irq, void *dev_id)
+{
+	struct vita_syscon *syscon = dev_id;
+
+	complete(&syscon->rx_irq);
+
+	return IRQ_HANDLED;
+}
+
+static int vita_syscon_probe(struct spi_device *spi)
+{
+	struct vita_syscon *syscon;
+	u8 baryon_version[SYSCON_RX_HEADER_SIZE + sizeof(syscon->baryon_version)];
+	u8 hw_info[SYSCON_RX_HEADER_SIZE + sizeof(syscon->hardware_info)];
+	u8 hw_flags[SYSCON_RX_HEADER_SIZE + sizeof(syscon->hardware_flags)];
+	int ret, irq;
+
+	pr_info("vita_syscon_probe\n");
+
+	syscon = devm_kzalloc(&spi->dev, sizeof(struct vita_syscon),
+				GFP_KERNEL);
+	if (!syscon)
+		return -ENOMEM;
+
+	irq = of_irq_get(spi->dev.of_node, 0);
+	if (irq <= 0)
+		return irq ? irq : -ENODEV;
+
+	ret = devm_request_irq(&spi->dev, irq, vita_syscon_rx_gpio_irq_handler, 0,
+			       "vita-syscon-gpio-rx", syscon);
+	if (ret) {
+		dev_err(&spi->dev, "could not request IRQ: %d\n", ret);
+		return ret;
+	}
+
+	syscon->tx_gpio = devm_gpiod_get(&spi->dev, "tx", GPIOD_OUT_LOW);
+	if (IS_ERR(syscon->tx_gpio))
+		return PTR_ERR(syscon->tx_gpio);
+
+	init_completion(&syscon->rx_irq);
+
+	spi_set_drvdata(spi, syscon);
+	syscon->dev = &spi->dev;
+	syscon->spi = spi;
+	syscon->command_read = vita_syscon_command_read;
+	syscon->short_command_write = vita_syscon_short_command_write;
+	syscon->transfer = vita_syscon_transfer;
+
+	ret = vita_syscon_command_read(syscon, 1, baryon_version, sizeof(baryon_version));
+	if (ret < 0) {
+		return ret;
+	}
+	memcpy(&syscon->baryon_version, &baryon_version[SYSCON_RX_DATA],
+	       sizeof(syscon->baryon_version));
+
+	pr_info("Vita Syscon Baryon version: 0x%X\n", syscon->baryon_version);
+
+	if (syscon->baryon_version > 0x1000003)
+		ret = vita_syscon_short_command_write(syscon, 0x80, 0x12, 3);
+	else if (syscon->baryon_version > 0x70501)
+		ret = vita_syscon_short_command_write(syscon, 0x80, 2, 3);
+	if (ret < 0) {
+		return ret;
+	}
+
+	ret = vita_syscon_command_read(syscon, 5, hw_info, sizeof(hw_info));
+	if (ret < 0) {
+		return ret;
+	}
+	memcpy(&syscon->hardware_info, &hw_info[SYSCON_RX_DATA], sizeof(syscon->hardware_info));
+
+	pr_info("Vita Syscon HW info: 0x%X\n", syscon->hardware_info);
+
+	ret = vita_syscon_command_read(syscon, 6, hw_flags, sizeof(hw_flags));
+	if (ret < 0) {
+		return ret;
+	}
+	memcpy(syscon->hardware_flags, &hw_flags[SYSCON_RX_DATA], sizeof(syscon->hardware_flags));
+
+	return devm_mfd_add_devices(syscon->dev, PLATFORM_DEVID_NONE,
+				    vita_syscon_devs, ARRAY_SIZE(vita_syscon_devs),
+				    NULL, 0, NULL);
+}
+
+static const struct of_device_id vita_syscon_of_match[] = {
+	{ .compatible = "vita,syscon" },
+	{}
+};
+
+MODULE_DEVICE_TABLE(of, vita_syscon_of_match);
+
+static struct spi_driver vita_syscon_driver = {
+	.driver = {
+		.name   = "vita-syscon",
+		.of_match_table = vita_syscon_of_match,
+	},
+	.probe = vita_syscon_probe,
+};
+
+module_spi_driver(vita_syscon_driver);
+
+MODULE_AUTHOR("Sergi Granell");
+MODULE_DESCRIPTION("PlayStation Vita's Syscon (Ernie) driver");
+MODULE_LICENSE("GPL");

drivers/spi/spi-vita.c
@@ -1,0 +1,234 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2021 Sergi Granell
+
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/device.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
+#include <linux/reset.h>
+#include <linux/spi/spi.h>
+
+#define SPI_RXFIFO	0x00
+#define SPI_TXFIFO	0x04
+#define SPI_CONTROL	0x08
+#define SPI_INT_CTL	0x0C
+#define SPI_STATUS	0x10
+#define SPI_UNK20	0x20
+#define SPI_INT_STAT	0x24
+#define SPI_RXFIFO_STAT	0x28
+#define SPI_TXFIFO_STAT	0x2C
+
+struct vita_spi {
+	struct spi_controller	*ctlr;
+	void __iomem		*regs;
+	struct clk		*clk;
+	struct reset_control	*rst;
+};
+
+static inline u32 vita_spi_read(struct vita_spi *vspi, u32 reg)
+{
+	u32 val = readl(vspi->regs + reg);
+	//pr_info("vita_spi_read: reg: 0x%02X, value: 0x%X\n", reg, val);
+	return val;
+}
+
+static inline void vita_spi_write(struct vita_spi *vspi, u32 reg, u32 value)
+{
+	//pr_info("vita_spi_write: reg: 0x%02X, value: 0x%X\n", reg, value);
+	writel(value, vspi->regs + reg);
+}
+
+static inline u32 vita_spi_read_fifo_available(struct vita_spi *vspi)
+{
+	return vita_spi_read(vspi, SPI_RXFIFO_STAT) & 0x7F;
+}
+
+static void vita_spi_transfer_send(struct vita_spi *vspi, const u8 *tx_buf, int len)
+{
+	u32 val;
+	unsigned int i = 0;
+
+	/* Flush pending data in the RX FIFO */
+	while (vita_spi_read_fifo_available(vspi) > 0)
+		vita_spi_read(vspi, SPI_RXFIFO);
+
+	vita_spi_read(vspi, SPI_TXFIFO_STAT);
+	vita_spi_write(vspi, SPI_INT_STAT, 0x600);
+
+	/* Transfer data */
+	while (len >= 2) {
+		val = tx_buf[i] | ((u32)tx_buf[i + 1] << 8);
+		vita_spi_write(vspi, SPI_TXFIFO, val);
+		i += 2;
+		len -= 2;
+	}
+
+	if (len > 0)
+		vita_spi_write(vspi, SPI_TXFIFO, tx_buf[i]);
+
+	/* Transfer end */
+	vita_spi_write(vspi, SPI_CONTROL, 0);
+	vita_spi_write(vspi, SPI_STATUS, 1);
+	vita_spi_read(vspi, SPI_STATUS);
+}
+
+static void vita_spi_transfer_recv(struct vita_spi *vspi, u8 *rx_buf, int len)
+{
+	u32 val, avail;
+	int i = 0;
+
+	while (i < len) {
+		avail = vita_spi_read_fifo_available(vspi);
+		if (!avail)
+			break;
+
+		val = vita_spi_read(vspi, SPI_RXFIFO);
+
+		if (avail >= 2 && ((i + 2) <= len)) {
+			rx_buf[i] = val & 0xFF;
+			rx_buf[i + 1] = (val >> 8) & 0xFF;
+			i += 2;
+		} else if (avail >= 1) {
+			rx_buf[i] = val & 0xFF;
+			i++;
+		}
+	}
+
+	/* Transfer end */
+	vita_spi_write(vspi, SPI_STATUS, 0);
+	vita_spi_read(vspi, SPI_STATUS);
+}
+
+static int vita_spi_transfer_one(struct spi_controller *ctlr,
+				 struct spi_device *spi,
+				 struct spi_transfer *tfr)
+{
+
+	struct vita_spi *vspi = spi_controller_get_devdata(ctlr);
+
+	//pr_info("vita_spi_transfer_one. tx_buf: 0x%X, rx_buf: 0x%X, len: 0x%X\n",
+	//       tfr->tx_buf, tfr->rx_buf, tfr->len);
+
+	if (tfr->rx_buf) {
+		vita_spi_transfer_recv(vspi, tfr->rx_buf, tfr->len);
+	} else {
+		vita_spi_transfer_send(vspi, tfr->tx_buf, tfr->len);
+	}
+
+	return 0;
+}
+
+static int vita_spi_probe(struct platform_device *pdev)
+{
+	struct spi_controller *ctlr;
+	struct vita_spi *vspi;
+	struct device *dev = &pdev->dev;
+	int ret = 0;
+
+	pr_info("vita_spi_probe\n");
+
+	ctlr = devm_spi_alloc_host(dev, sizeof(struct vita_spi));
+	if (!ctlr)
+		return -ENOMEM;
+
+	platform_set_drvdata(pdev, ctlr);
+
+	ctlr->transfer_one = vita_spi_transfer_one;
+	ctlr->mode_bits = SPI_CPOL | SPI_CPHA | SPI_CS_HIGH | SPI_LSB_FIRST;
+	ctlr->bits_per_word_mask = SPI_BPW_MASK(8);
+	ctlr->dev.of_node = pdev->dev.of_node;
+	ctlr->auto_runtime_pm = true;
+
+	vspi = spi_controller_get_devdata(ctlr);
+	vspi->ctlr = ctlr;
+
+	vspi->regs = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(vspi->regs))
+		return PTR_ERR(vspi->regs);
+
+	vspi->clk = devm_clk_get_optional(dev, NULL);
+	if (IS_ERR(vspi->clk))
+		return dev_err_probe(dev, PTR_ERR(vspi->clk),
+				     "could not get clk\n");
+
+	clk_prepare_enable(vspi->clk);
+
+	vspi->rst = devm_reset_control_get_exclusive(dev, NULL);
+	if (IS_ERR(vspi->rst)) {
+		ret = PTR_ERR(vspi->rst);
+		goto err_disable_clk;
+	}
+	reset_control_deassert(vspi->rst);
+
+	vita_spi_write(vspi, SPI_UNK20, 0);
+	vita_spi_read(vspi, SPI_UNK20);
+
+	pm_runtime_enable(dev);
+	pm_runtime_set_active(dev);
+
+	ret = spi_register_controller(ctlr);
+	if (ret) {
+		dev_err(dev, "could not register SPI controller: %d\n",
+			ret);
+		goto err_reset;
+	}
+
+	return 0;
+
+err_reset:
+	reset_control_assert(vspi->rst);
+
+err_disable_clk:
+	clk_disable_unprepare(vspi->clk);
+
+	return ret;
+}
+
+static void vita_spi_remove(struct platform_device *pdev)
+{
+	struct spi_controller *ctlr = platform_get_drvdata(pdev);
+	struct vita_spi *vspi = spi_controller_get_devdata(ctlr);
+	struct device *dev = &pdev->dev;
+
+	pm_runtime_get_sync(dev);
+
+	reset_control_assert(vspi->rst);
+
+	clk_disable_unprepare(vspi->clk);
+
+	pm_runtime_disable(dev);
+	pm_runtime_put_noidle(dev);
+
+	spi_unregister_controller(ctlr);
+}
+
+static void vita_spi_shutdown(struct platform_device *pdev)
+{
+	vita_spi_remove(pdev);
+}
+
+static const struct of_device_id vita_spi_match[] = {
+	{ .compatible = "vita,spi", },
+	{}
+};
+MODULE_DEVICE_TABLE(of, vita_spi_match);
+
+static struct platform_driver vita_spi_driver = {
+	.driver	= {
+		.name		= "vita-spi",
+		.of_match_table	= vita_spi_match,
+	},
+	.probe		= vita_spi_probe,
+	.remove		= vita_spi_remove,
+	.shutdown	= vita_spi_shutdown,
+};
+module_platform_driver(vita_spi_driver);
+
+MODULE_AUTHOR("Sergi Granell");
+MODULE_DESCRIPTION("PlayStation Vita SPI driver");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:vita-spi");